792818fc43ed82e303569f412becb991cbaa8817
Yan-Zer0 <yanpeipan_82@qq.com>
2020-07-02 01:35:12 +0800

    Create gulpfile.ts

---
+++ gulpfile.ts
@@ -0,0 +1,12 @@
+const { series, parallel } = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+
+
+function build() {
+    gulp.src('_site/assets/**/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('_site/assets/'))
+}
+
+
+exports.default = build
