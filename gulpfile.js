5c6e53fc5f29d83d1a89507c8e5bc588e65e83ee
Yan-Zer0 <yanpeipan_82@qq.com>
2020-07-02 02:51:44 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,10 @@ const cleanCSS = require('gulp-clean-css');
 
 function build() {
     return gulp.src('_site/assets/**/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(cleanCSS({debug: true}, (details) => {
+      console.log(`${details.name}: ${details.stats.originalSize}`);
+      console.log(`${details.name}: ${details.stats.minifiedSize}`);
+    }))
     .pipe(gulp.dest('_site/assets/'))
 }
 
