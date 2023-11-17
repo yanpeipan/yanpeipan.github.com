0ccc4f2454ad7041ed4bf46b03c3f466c70ba67a
yanpeipan <yanpeipan@yans-Mac-mini>
2023-11-17 19:43:40 +0800

    feat: deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,6 +27,7 @@ const config = {
   deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+  trailingSlash: false,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
